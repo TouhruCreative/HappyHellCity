eef7e9978f6b539bb369056d3b99d0e076c40ff5
TouhruCreative <touhru.shott@gmail.com>
2023-09-29 21:47:21 +0600

    Add resuorce - Happy; Update Bottom Button

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -19,6 +19,10 @@ public class PlayerScript : MonoBehaviour, IInteractable
 
     public float nowMaterial = 10000;
     public GameObject TextMaterial;
+    public float nowHappy=0;
+    public GameObject TextHappy;
+    public GameObject NN;
+
 
     public string GetDescription()
     {
@@ -34,49 +38,10 @@ public class PlayerScript : MonoBehaviour, IInteractable
     void Update()
     {   
         update_UI();
-        // if(Input.GetKey(KeyCode.W))
-        // {
-        //     player.transform.position += player.transform.forward * speed * Time.deltaTime;
-        // }
-        // if(Input.GetKey(KeyCode.S))
-        // {
-        //     player.transform.position -= player.transform.forward * speed * Time.deltaTime;
-        // }
-        // if(Input.GetKey(KeyCode.D))
-        // {
-        //     player.transform.position += player.transform.right * speed * Time.deltaTime;
-        //     }
-        // if(Input.GetKey(KeyCode.A))
-        // {
-        //     player.transform.position -= player.transform.right * speed * Time.deltaTime;
-        // }
-        // if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, -90-45, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, 90+45, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, -45, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, +45, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.W)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, 180, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.D)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, -90, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.A)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, 90, 0);
-        // }
-        // else if(Input.GetKey(KeyCode.S)){
-        //     player_t.transform.rotation = Quaternion.Euler(0, 0, 0);
-        // }
     }
     public void update_UI(){
         TextCash.GetComponent<UnityEngine.UI.Text>().text=nowCash.ToString();
         TextMaterial.GetComponent<UnityEngine.UI.Text>().text=nowMaterial.ToString();
+        TextHappy.GetComponent<UnityEngine.UI.Text>().text=nowHappy.ToString();
     }
 }
